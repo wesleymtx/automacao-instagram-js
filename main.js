4ef6335da23e2c7b2a5e7baaae769c24ea815632
Wesley Gimenez <50761161+wesleymtx@users.noreply.github.com>
2020-11-17 16:43:43 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+let  likes = 0;
+let intervalTime = 15000;
+function heartClick(){
+        intervalTime = 10000;
+        intervalTime += Math.random()*20000;
+        intervalHeart = Math.random()*3000+2000;
+        intervalArrow = Math.random()*4000+5001;
+
+        console.log(intervalTime); 
+        const heart = document.querySelector('svg[aria-label="Gosto"]').parentNode; 
+        const arrow = document.querySelector('a.coreSpriteRightPaginationArrow');
+
+        setTimeout(function(){
+                heart.click();
+                likes++;
+                console.log("like num: "+likes+" IntervalHeart: "+intervalHeart)
+        }, intervalHeart)
+        
+        setTimeout(function(){
+                arrow.click();
+                console.log("Invervalo da arrow: "+intervalArrow)
+        }, intervalArrow)
+}
+
+setInterval(heartClick, intervalTime);
